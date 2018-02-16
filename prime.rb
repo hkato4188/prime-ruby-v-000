3b82d8ad1b4e2f03c1fc00947f2fdfc4e24ddcd2
Hiroki Kato <hkato@sas.upenn.edu>
2018-02-16 15:06:16 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,9 @@
 def prime?(number)
-  if number < 0 || number == 0 || number == 1
+  if number < 0 or number == 0 or number == 1
     return false
   else
     (2..number-1).to_a.all? do |possible_factor|
-      number % possible_factor != 0
+      num % possible_factor != 0
     end
   end
 end
